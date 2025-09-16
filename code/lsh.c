47e0b0d681967f1972a51a3fbce6b9c5c934f57e
Fakedy <jimmydaswag@hotmail.com>
2025-09-16 14:21:48 +0200

    Added ls command

--- code/lsh.c
+++ code/lsh.c
@@ -165,6 +165,15 @@ void run_prgm(Pgm *p, int* get_child_pid, unsigned char flags) {
           exit(EXIT_FAILURE);
         }
       }
+
+      if(strcmp(argv[0], "ls") == 0) {
+        if(execvp(argv[0],argv) == -1){
+          perror("execvp failed");
+          exit(EXIT_FAILURE);
+        }
+      }
+
+
     }
     else {
       bool is_background = flags & FLAG_BACKGROUND;
